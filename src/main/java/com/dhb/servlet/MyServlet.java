7298768d1f943a997709d73849d2a69d1a19c5aa
haibo.duan <dhaibo1986@sohu.com>
2021-02-22 14:16:30 +0800

    自定义一个servlet

---
+++ src/main/java/com/dhb/servlet/MyServlet.java
@@ -0,0 +1,28 @@
+package com.dhb.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "myServlet",urlPatterns = "/srv")
+public class MyServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("servlet doGet method called...");
+		resp.getWriter().println("doGet method called ...");
+		resp.getWriter().flush();
+		resp.getWriter().close();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("servlet doPost method called...");
+		resp.getWriter().println("doPost method called ...");
+		resp.getWriter().flush();
+		resp.getWriter().close();
+	}
+}
