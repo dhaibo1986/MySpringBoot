7b470cd992c61db33d02d8c7e460b3bd820a8b4a
haibo.duan <dhaibo1986@sohu.com>
2021-02-23 15:58:49 +0800

    修改Controller 增加session处理

--- src/main/java/com/dhb/controller/HelloController.java
+++ src/main/java/com/dhb/controller/HelloController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
 	@ResponseBody
-	public String hello() {
+	public String hello(HttpSession session) {
+		session.setAttribute("aa","aa");
 		return "hello";
 	}
 
--- src/main/java/com/dhb/controller/RestHelloController.java
+++ src/main/java/com/dhb/controller/RestHelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestHelloController {
 	@RequestMapping("/resthello")
 	public String hello() {
+		System.out.println("hello ... ...");
 		return "hello word!";
 	}
 }
