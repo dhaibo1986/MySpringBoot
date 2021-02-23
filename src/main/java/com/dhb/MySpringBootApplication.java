12926d7fd36ea416c97d1d9890b382d3460df9a5
haibo.duan <dhaibo1986@sohu.com>
2021-02-23 16:00:07 +0800

    增加监听器 过滤器之后的启动类

--- src/main/java/com/dhb/MySpringBootApplication.java
+++ src/main/java/com/dhb/MySpringBootApplication.java
@@ -1,9 +1,11 @@
 package com.dhb;
 
 import com.dhb.servlet.MyServlet;
+import com.dhb.servlet.listener.MyHttpSessionListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
@@ -18,8 +20,16 @@ public class MySpringBootApplication {
 	//将servlet 添加到springboot容器中
 	@Bean
 	public ServletRegistrationBean<MyServlet> webServletRegistrationBean() {
-		ServletRegistrationBean<MyServlet> bean = new ServletRegistrationBean<>(new MyServlet());
+		ServletRegistrationBean<MyServlet> bean = new ServletRegistrationBean<>(new MyServlet(),"/s2");
 		bean.setLoadOnStartup(1);
 		return bean;
+	}
+	
+	@Bean
+	public ServletListenerRegistrationBean listenerRegistrationBean() {
+		ServletListenerRegistrationBean srb = new ServletListenerRegistrationBean();
+		srb.setListener(new MyHttpSessionListener());
+		System.out.println("listener 创建成功");
+		return srb;
 	}
 }
