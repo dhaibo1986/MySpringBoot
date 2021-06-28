63a6ab18079a1564c7d1838efcb2bbc48b87994a
haibo.duan <dhaibo1986@sohu.com>
2021-06-28 19:51:43 +0800

    删除错误类

--- src/main/java/com/dhb/config/MyConfig.java
+++ src/main/java/com/dhb/config/MyConfig.java
@@ -1,10 +1,8 @@
 package com.dhb.config;
 
-import org.apache.tomcat.util.descriptor.LocalResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,29 +13,35 @@ import java.util.Locale;
 
 @Configuration
 public class MyConfig implements WebMvcConfigurer {
-	
+
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/dhb").setViewName("hello");
+	}
 	
 	@Bean
 	public LocaleResolver localeResolver() {
 		return new NativeLocaleResolver();
 	}
-	
+
 	protected static class NativeLocaleResolver implements LocaleResolver {
 		
 		@Override
 		public Locale resolveLocale(HttpServletRequest request) {
 			String language = request.getParameter("language");
+			System.out.println("language is: " + language);
 			Locale locale = Locale.getDefault();
-			if(!ObjectUtils.isEmpty(language)){
+			if (!ObjectUtils.isEmpty(language)) {
 				String[] split = language.split("_");
-				locale = new Locale(split[0],split[1]);
+				locale = new Locale(split[0], split[1]);
 			}
 			return locale;
 		}
 
 		@Override
 		public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
-			
+
 		}
 	}
 }
